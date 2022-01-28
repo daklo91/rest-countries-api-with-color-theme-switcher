e81a34d8c19cfa0d6be8d58867c94af1bef412c2
daklo91 <coffeedog91@gmail.com>
2022-01-28 13:07:22 +0100

    Update darkmode. Add button and text

--- src/components/Header/DarkMode.ts
+++ src/components/Header/DarkMode.ts
@@ -15,19 +15,23 @@ const toggleTheme = () => {
   }
 };
 
-const darkModeIcon: string = /* html */ `<svg
-  class="dark-mode-icon"
-  onclick="toggleTheme()"
-  width="16"
-  height="16"
-  viewBox="0 0 16 16"
-  fill="none"
-  xmlns="http://www.w3.org/2000/svg">
-  <path
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-    d="M10.8426 11.052C7.73486 11.052 5.21543 8.74226 5.21543 5.89457C5.21543 4.82024 5.57343 3.82526 6.18514 3C3.75229 3.75612 2 5.86498 2 8.35045C2 11.4708 4.75943 14 8.16286 14C10.8743 14 13.1757 12.3945 14 10.1636C13.1 10.7238 12.0129 11.052 10.8426 11.052Z"
-    fill="white"
-    stroke="#111517"/>
-</svg>
+const darkModeIcon: string = /* html */ `
+<button class="dark-mode-button" onclick="toggleTheme()">
+  <svg
+    class="dark-mode-icon"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+      <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M10.8426 11.052C7.73486 11.052 5.21543 8.74226 5.21543 5.89457C5.21543 4.82024 5.57343 3.82526 6.18514 3C3.75229 3.75612 2 5.86498 2 8.35045C2 11.4708 4.75943 14 8.16286 14C10.8743 14 13.1757 12.3945 14 10.1636C13.1 10.7238 12.0129 11.052 10.8426 11.052Z"
+      fill="none"
+      stroke="none"
+      stroke-width="none"/>
+  </svg> 
+  <span class="dark-mode-text">Dark Mode</span>
+</button>
 `;
