1f9f8a61ae76866098203097a42b5568287e2342
daklo91 <coffeedog91@gmail.com>
2022-01-28 13:06:05 +0100

    Remove div and text I want these in the darkmode file so everything "darkmode" is there

--- src/components/Header/Header.ts
+++ src/components/Header/Header.ts
@@ -2,7 +2,7 @@ const header = (): string => {
   return /* html */ `
   <header>
     <h1>Where in the world?</h1>
-    <div>${darkModeIcon} Dark Mode</div>
+    ${darkModeIcon}
   </header>`;
 };
 
